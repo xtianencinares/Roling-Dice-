60317af79d4a059a4cb610a4347a897e4e87fded
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2024-06-03 09:57:35 +0800

    Update Agoo Dice.py

--- Agoo Dice.py
+++ Agoo Dice.py
@@ -20,5 +20,7 @@ while roll_again == "yes" or roll_again == "y":
     #generating and printing 2nd random integer from 1 to 6
     print(random.randint(min_val, max_val))
     
- #asking user to roll the dice again. Any input other than yes or y will terminate the loop
   roll_again = input("Agoo Roll the Dices Again?")  
+
+
+#Agoo.Inc & MSFT 2024
